17a96ca4367bc5f4ff599b51936ed5d397359879
abhinand <abhinandc293@gmail.com>
2025-04-09 22:53:12 +0530

    error_fixed

--- connection.js
+++ connection.js
@@ -6,8 +6,7 @@ dotenv.config();
 export default async function connection() {
     try {
         const db = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true, 
+           
         });
         console.log("Database connected to Atlas");
         return db;
