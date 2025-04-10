6d13a81a42919ddfa108438d2c1886f2d9eb02c9
abhinand <abhinandc293@gmail.com>
2025-04-10 07:01:19 +0530

    .

--- server.js
+++ server.js
@@ -9,14 +9,9 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: ["https://restaurant-abhinand.vercel.app", "http://localhost:5177"], 
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
-  credentials: true,
-};
 
-app.use(cors(corsOptions));
-app.use(express.json({ limit: "50mb" }));
+app.use(cors());
+app.use(express.json({ limit: "100mb" }));
 app.use("/api", Router);
 
 const PORT = process.env.PORT || 3000;
